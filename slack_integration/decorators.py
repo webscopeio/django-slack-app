e36c564075f841cd56c3f77428be9648e6732df5
Jan Vorcak <vorcak@webscope.io>
2020-02-08 01:01:01 +0100

    Slack Signing Secret implementation

--- slack_integration/decorators.py
+++ slack_integration/decorators.py
@@ -1,1 +1,38 @@
-# TODO - write a decorator for hooks incluing security stuff
+from django.http import JsonResponse
+from django.urls import reverse
+
+from .models import SlackWorkspace, SlackUserMapping
+
+
+def require_linked_slack_account():
+    """
+    Checks whether user has linked his/her Slack account with User model
+    """
+
+    def decorator(custom_method):
+        def _decorator(self, *args, **kwargs):
+            team_id = self.POST.get('team_id')
+            try:
+                workspace = SlackWorkspace.objects.get(id=team_id)
+            except SlackWorkspace.DoesNotExist:
+                return JsonResponse({
+                    "text": "Slack application is not linked to your workspace. " +
+                            "Please ask your administrators to finish the installation."
+                })
+
+            user_id = self.POST.get('user_id')
+            mapping, created = SlackUserMapping.objects.get_or_create(slack_user_id=user_id, slack_workspace=workspace)
+
+            if created or mapping.user is None:
+                # url = reverse('slack:bind-accounts', kwargs={'nonce': mapping.nonce})
+                url = "https://example.com"
+                return JsonResponse({
+                    "text": "Hi, it seems like you haven't linked your Slack account to your Scrumie account. " +
+                            f"You can do so <{self.build_absolute_uri(url)}|here>"
+                })
+
+            return custom_method(self, mapping, workspace, *args, **kwargs)
+
+        return _decorator
+
+    return decorator
--- slack_integration/helpers.py
+++ slack_integration/helpers.py
@@ -1,5 +1,12 @@
 import datetime
+import hashlib
+import hmac
+import time
 from typing import Tuple
+
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.http import HttpRequest
 
 from .models import SlackWorkspace, SlackWebHook, SlackUserMapping
 
@@ -32,3 +39,36 @@ def create_workspace_from_oauth2_response(request_user, response) -> Tuple[Slack
     )
 
     return workspace, hook
+
+
+def is_verified_slack_request(request: HttpRequest) -> bool:
+    """
+    Implements Slack signature verification according to
+    https://api.slack.com/docs/verifying-requests-from-slack
+
+    :param request: Django request
+    :return: True if request is verified, False otherwise
+    """
+    try:
+        slack_signing_secret = settings.SLACK_SIGNING_SECRET
+    except AttributeError:
+        raise ImproperlyConfigured(
+            "SLACK_SIGNING_SECRET is missing in your settings.py" +
+            "Please check the readme of django-slack-integration package"
+        )
+
+    slack_signature = request.META.get('HTTP_X_SLACK_SIGNATURE')
+    slack_request_timestamp = request.META.get('HTTP_X_SLACK_REQUEST_TIMESTAMP')
+
+    if not slack_signature or not slack_request_timestamp:
+        return False
+
+    if abs(time.time() - int(slack_request_timestamp)) > 60 * 5:
+        # The request timestamp is more than five minutes from local time.
+        # It could be a replay attack, so let's ignore it.
+        return False
+
+    sig_basestring = f"v0:{slack_request_timestamp}:{request.body.decode('utf-8')}".encode("utf-8")
+    my_signature = 'v0=' + hmac.new(bytes(slack_signing_secret, 'utf-8'), sig_basestring, hashlib.sha256).hexdigest()
+
+    return hmac.compare_digest(my_signature, slack_signature)
